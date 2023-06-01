66c8dc68cd33f43a19e53ae7f58e64c500920602
David Ward <spy.sappin.your.sentry@gmail.com>
2023-06-01 13:38:11 -0700

    Fully remove download button temporarily to fix linter

--- src/components/SaveConverter.tsx
+++ src/components/SaveConverter.tsx
@@ -37,42 +37,42 @@ export default function SaveConverter() {
     fileReader.readAsArrayBuffer(file);
   };
 
-  const downloadSaveHandler = () => {
-    downloadFile({
-      data: JSON.stringify(saveData, null, 2),
-      fileName: "fapi-save-json.json",
-      fileType: "text/json",
-    });
-  };
+  // const downloadSaveHandler = () => {
+  //   downloadFile({
+  //     data: JSON.stringify(saveData, null, 2),
+  //     fileName: "fapi-save-json.json",
+  //     fileType: "text/json",
+  //   });
+  // };
 
   const removeSaveHandler = () => {
     setSaveData({});
   };
 
-  const downloadFile = ({
-    data,
-    fileName,
-    fileType,
-  }: {
-    data: string;
-    fileName: string;
-    fileType: string;
-  }) => {
-    // Create a blob with the data we want to download as a file
-    const blob = new Blob([data], { type: fileType });
-    // Create an anchor element and dispatch a click event on it
-    // to trigger a download
-    const a = document.createElement("a");
-    a.download = fileName;
-    a.href = window.URL.createObjectURL(blob);
-    const clickEvt = new MouseEvent("click", {
-      view: window,
-      bubbles: true,
-      cancelable: true,
-    });
-    a.dispatchEvent(clickEvt);
-    a.remove();
-  };
+  // const downloadFile = ({
+  //   data,
+  //   fileName,
+  //   fileType,
+  // }: {
+  //   data: string;
+  //   fileName: string;
+  //   fileType: string;
+  // }) => {
+  //   // Create a blob with the data we want to download as a file
+  //   const blob = new Blob([data], { type: fileType });
+  //   // Create an anchor element and dispatch a click event on it
+  //   // to trigger a download
+  //   const a = document.createElement("a");
+  //   a.download = fileName;
+  //   a.href = window.URL.createObjectURL(blob);
+  //   const clickEvt = new MouseEvent("click", {
+  //     view: window,
+  //     bubbles: true,
+  //     cancelable: true,
+  //   });
+  //   a.dispatchEvent(clickEvt);
+  //   a.remove();
+  // };
 
   const uploadFileButtonHandler = () => {
     document.getElementById("uploadFile")?.click();
